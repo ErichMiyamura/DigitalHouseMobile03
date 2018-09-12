821d0ade3823184d24634e4f31c89c97158232ec
unknown <andres@digitalhouse.com>
2018-09-11 22:30:44 -0300

    Aula 18

--- Android/Aula16/app/src/main/java/br/com/digitalhouse/app/SplashActivity.java
+++ Android/Aula16/app/src/main/java/br/com/digitalhouse/app/SplashActivity.java
@@ -1,14 +1,27 @@
 package br.com.digitalhouse.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private ImageView imageViewSplash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        imageViewSplash = (ImageView) findViewById(R.id.imageview_splash); // retorna uma instacia de uma view
+        imageViewSplash.setOnClickListener(new View.OnClickListener() { // ouvir um evento de click
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            }
+        });
     }
 
     @Override
